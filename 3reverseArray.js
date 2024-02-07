3626e7dc78219e44f7784c2ff729478a44d9fae6
Anastasiya Vashkevich <a.vashkevi4@gmail.com>
2024-02-07 18:32:08 +0300

    add file dicegame, action with array, compare numbers in numbers

---
+++ 3reverseArray.js
@@ -0,0 +1,6 @@
+// Поменять массив в обратном порядке - [1,2,3,4,5,6] [6,5,4,3,2,1]
+
+let array = [1, 2, 3, 4, 5, 6];
+let array2 = ['Anastasiya', 'Vashkevich', 32, true, "QA"]
+console.log(array.reverse());
+console.log(array2.reverse());
